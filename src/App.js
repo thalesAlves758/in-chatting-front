50ddab9bb954e8ed777d985d260c1917e4eb0644
Thales Alves <thalesalves758@gmail.com>
2022-10-07 20:50:56 -0300

    feat: create routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <SignIn />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/sign-in' element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
