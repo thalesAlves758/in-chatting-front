0a1805e9d76e6daf7613993017807c1ed3ae4acf
Thales Alves <thalesalves758@gmail.com>
2022-10-07 18:40:01 -0300

    feat: create Auth Form Container layout component

---
+++ src/components/layout/AuthFormContainer.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const AuthFormContainer = styled.div`
+  border-radius: 3px;
+  padding: 15px;
+  background-color: #e3e6e6;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default AuthFormContainer;
