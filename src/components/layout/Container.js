dc29e747d1bca2fb2c4b2bf11e1e5817182bd921
Thales Alves <thalesalves758@gmail.com>
2022-10-07 20:49:53 -0300

    fix: accept props

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: ${(props) => props.justify ? props.justify : 'flex-start'};
+  align-items: ${(props) => props.align ? props.align : 'stretch'};
+  padding: ${(props) => props.padding ? props.padding : '0px'};
 `;
 
 export default Container;
