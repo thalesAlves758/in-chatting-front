c03b76b839fab5d45deb891a83515785f1e815da
Thales Alves <thalesalves758@gmail.com>
2022-10-10 18:35:00 -0300

    feat: add user context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import UserContext from './contexts/UserContext';
+import { useLocalStorage } from './hooks/useLocalStorage';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 function App() {
+  const [userStorage, setUserStorage] = useLocalStorage('user', null);
+
+  const [user, setUser] = useState(userStorage);
+
+  useEffect(() => {
+    setUserStorage(user);
+  }, [user, setUserStorage]);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/sign-in' element={<SignIn />} />
-      </Routes>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Routes>
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/sign-in' element={<SignIn />} />
+        </Routes>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
