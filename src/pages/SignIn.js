615f98c77a345468226b154b28663ddb3e449508
Thales Alves <thalesalves758@gmail.com>
2022-10-10 18:34:34 -0300

    feat: reformulate sign in page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,61 +1,105 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { Button, Form, Input, message, Row, Space } from 'antd';
 import styled from 'styled-components';
-import { Row, Col, Form, Input, Button, Space } from 'antd';
-import AuthFormContainer from '../components/layout/AuthFormContainer';
 import Container from '../components/layout/Container';
-import Logo from '../components/layout/Logo';
+import AppInfo from '../components/AppInfo';
+import AuthContainerCol from '../components/layout/AuthContainerCol';
 import { useNavigate } from 'react-router-dom';
+import Logo from '../components/layout/Logo';
+import axios from 'axios';
+import UserContext from '../contexts/UserContext';
 
-const SignIn = () => {
-  const navigate = useNavigate();
-
+const SignInForm = () => {
   const [form] = Form.useForm();
 
   const [isLoading, setIsLoading] = useState(false);
 
-  const onFinish = () => {
+  const { setUser } = useContext(UserContext);
+
+  const showErrorMessage = (errorMessage) => {
+    message.error(errorMessage);
+  }
+
+  const getErrorMessageByStatusCode = (statusCode) => {
+    const hashErrors = {
+      401: 'Email ou senha incorretos!',
+    }
+
+    return hashErrors[statusCode] || 'Erro desconhecido';
+  }
+
+  const onFinish = async () => {
     setIsLoading(true);
 
-    console.log(form.getFieldsValue());
+    try {
+      const { data: user } = await axios.post('http://localhost:5000/sign-in', {
+        ...form.getFieldsValue()
+      });
+
+      setUser(user);
+      setIsLoading(false);
+    } catch (error) {
+      let errorMessage = 'Não foi possível concluir a operação';
+
+      if (error.response) {
+        errorMessage = getErrorMessageByStatusCode(error.response.status);
+      }
+
+      showErrorMessage(errorMessage);
+      setIsLoading(false);
+    }
   }
 
   return (
-    <Container justify={'center'}>
-      <Row justify='center'>
-        <Col lg={6} span={20}>
-          <AuthFormContainer>
-            <Logo />
+    <StyledForm form={form} onFinish={onFinish} name='sign-in' autoComplete='off' layout='vertical' requiredMark={false}>
+      <LogoContainer>
+        <Logo small={true} />
+      </LogoContainer>
 
-            <StyledForm onFinish={onFinish} form={form} autoComplete='off' name='signIn' layout='vertical' requiredMark={false}>
-              <Form.Item label="E-mail:" name="email" rules={[
-                { required: true, message: 'Por favor, insira um e-mail válido!', type: 'email' }
-              ]}>
-                <Input disabled={isLoading} placeholder='Digite seu email...' />
-              </Form.Item>
+      <Form.Item label="E-mail:" name="email" rules={[
+        { required: true, message: 'Por favor, insira um e-mail válido!', type: 'email' }
+      ]}>
+        <Input disabled={isLoading} placeholder='Digite seu email...' />
+      </Form.Item>
 
-              <Form.Item label="Senha:" name="password" rules={[
-                { required: true, message: 'Por favor, insira sua senha!' }
-              ]}>
-                <Input.Password disabled={isLoading} placeholder="Digite a senha..." />
-              </Form.Item>
+      <Form.Item label="Senha:" name="password" rules={[
+        { required: true, message: 'Por favor, insira sua senha!' }
+      ]}>
+        <Input.Password disabled={isLoading} placeholder="Digite a senha..." />
+      </Form.Item>
 
-              <StyledButton loading={isLoading} type="primary" htmlType="submit">
-                Entrar
-              </StyledButton>
+      <StyledButton loading={isLoading} type="primary" htmlType="submit">
+        Entrar
+      </StyledButton>
+    </StyledForm>
+  );
+}
 
-              <StyledSpace direction='vertical' align='center'>
-                Não possui uma conta?<Button onClick={() => navigate('/sign-up')} type="link">Cadastre-se</Button>
-              </StyledSpace>
-            </StyledForm>
-          </AuthFormContainer>
-        </Col>
-      </Row>
+const SignIn = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <Display>
+        <AppInfo />
+        <AuthContainerCol lg={6} span={24}>
+          <SignInForm />
+
+          <Space direction='vertical' align='center'>
+            Não possui uma conta?<Button onClick={() => navigate('/sign-up')} type="link">Cadastre-se</Button>
+          </Space>
+        </AuthContainerCol>
+      </Display>
     </Container>
   );
 }
 
+const Display = styled(Row)`
+  height: 100vh;
+`;
+
 const StyledForm = styled(Form)`
-  margin: 20px 0;
+  margin-bottom: 20px;
   display: flex;
   flex-direction: column;
 `;
@@ -65,11 +109,17 @@ const StyledButton = styled(Button)`
   margin: 0 auto;
   background-color: #284B63;
   border: 1px solid #284B63;
-  border-radius: 3px;
+  border-radius: 5px;
 `;
 
-const StyledSpace = styled(Space)`
-  margin-top: 20px;
+const LogoContainer = styled.div`
+  @media (min-width: 990px) {
+    display: none;
+  }
+
+  display: flex;
+  justify-content: center;
+  margin: 20px 0;
 `;
 
 export default SignIn;
