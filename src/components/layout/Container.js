3c82a3ee063a47943373e4ac5a25cf4d8edbedf6
Thales Alves <thalesalves758@gmail.com>
2022-10-10 18:32:37 -0300

    fix: change Container style

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -3,11 +3,6 @@ import styled from 'styled-components';
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: ${(props) => props.justify ? props.justify : 'flex-start'};
-  align-items: ${(props) => props.align ? props.align : 'stretch'};
-  padding: ${(props) => props.padding ? props.padding : '0px'};
 `;
 
 export default Container;
